94e135c849032da6b65462a0369ca90421f0e83a
CECurtiss <curtissclinton@gmail.com>
2022-11-30 21:41:14 -0600

    created get for notes.html

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
-const express = require('express');
+const express = require("express");
+const path = require('path');
 
-const noteData = require('./db/db.json');
+const noteData = require("./db/db.json");
 
 const PORT = 3001;
 
 const app = express();
 
-app.post('/db/db.json', (req,res) => {
-    res.json(`${req.method} request received`);
+app.use(express.static('public'));
 
-    console.info(req.rawHeaders);
-
-    console.info(`${req.method} request received`);
+app.get("/", (req, res) => {
+  res.send("Navigate to notes.html");
 });
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "public/notes.html"))
+});
+
+app.post("/db/db.json", (req, res) => {
+  res.json(`${req.method} request received`);
+
+  console.info(req.rawHeaders);
+
+  console.info(`${req.method} request received`);
+});
+
+app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
