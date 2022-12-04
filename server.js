cbd75191fb25a01c31a9ee3cdb62b4d0145ceb64
CECurtiss <curtissclinton@gmail.com>
2022-12-03 20:19:06 -0600

    working on how to post notes to page

--- server.js
+++ server.js
@@ -11,39 +11,32 @@ app.use(express.static('public'));
 app.use(express.json())
 app.use(express.urlencoded( { extended: true }))
 
-app.get("/", (req, res) => {
-  res.send("Navigate to notes.html");
-});
+// route to get main page
+// app.get("/", (req, res) => {
+//   res.send("Navigate to notes.html");
+// });
 
+// route to notes page
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, "public/notes.html"))
 });
 
+// route to post notes to db
 app.post("/api/notes", (req, res) => {
-    let response;
+    const newNote = req.body;
+    
 
-    if (req.body && req.body.product) {
-        response = {
-            status: 'Success',
-            data: req.body,
-        };
-        res.json(response);
-    } else {
-        res.json( {
-            status: 'error',
-            data: null,
-            message: 'Product required'
-        })
-    }
-    console.log(req.body)
+
+    
 });
 
 app.get('/api/notes', (req,res) => res.json(noteData));
 
+// route to main page
 app.get("*", (req,res) => {
     res.sendFile(path.join(__dirname, "public/index.html"))
 })
 
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
 
-1:01:56 express.js day 2
+// 1:01:56 express.js day 2
